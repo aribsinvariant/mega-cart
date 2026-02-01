32d3fc6641bb27ebccd375394f1668aed1be9ae9
Xavier Ibanez-Padron <xavijandro@gmail.com>
2026-02-01 02:25:08 -0500

    Create table initialization sql file, began logic for user registration.

--- mega-cart/auth-service/index.js
+++ mega-cart/auth-service/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const amqp = require('amqplib');
 const db = require('./db');
-const { query } = require('./db');
+const bcrypt = require('bcrypt');
+// const { query } = require('./db');
 const app = express();
 app.use(express.json());
 
@@ -15,8 +16,22 @@ async function connectQueue() {
 }
 connectQueue();
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Check if user already exists
+    const result = await db.query('SELECT * FROM users WHERE username = $1', [username]);
+    if (result.rows.length > 0) {
+        // username is taken
+    } else {
+        // user available
+    }
     
+
+
 });
 
 app.post('/login', (req, res) => {

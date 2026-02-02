df386200b621f1a705d6026c77f80af9d91c714d
damian7270 <damian7270majewski@gmail.com>
2026-02-01 19:59:18 -0500

    Made a navbar for the landing page

--- mega-cart/frontend/src/App.vue
+++ mega-cart/frontend/src/App.vue
@@ -1,10 +1,40 @@
-<script setup></script>
-
 <template>
-  <h1>Landing Page</h1>
-  <p>
-    This is the landing page. now we need to add stuff
-  </p>
+    <navbar
+        :pages="pages"
+        :active-page="activePage"
+        :nav-link-click="(index) => activePage = index"
+    ></navbar>
+    
+    <page-viewer 
+        :page="pages[activePage]"
+    ></page-viewer>
 </template>
 
-<style scoped></style>
+<script>
+import Navbar from './components/Navbar.vue';
+import PageViewer from './components/PageViewer.vue';
+
+export default {
+    components: {
+        Navbar,
+        PageViewer
+    },
+    data() {
+        return {
+            activePage: 0,
+            pages: [
+                {
+                    link: {text: 'Home', url: 'index.html'},
+                    pageTitle: 'Home Page',
+                    content: 'This is the home landing page'
+                },
+                {
+                    link: {text: 'Sign up', url: 'about.html'},
+                    pageTitle: 'Carts Page',
+                    content: 'This is the sign up page, it is currently empty'
+                }
+            ]
+        };
+    },
+}
+</script>

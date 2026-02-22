afe2c747bd0a0298a1693bcae74956e81374106b
linwei35 <marcow.lin@mail.utoronto.ca>
2026-02-21 19:04:00 -0500

    fixed at sign break share link function

--- mega-cart/frontend/src/i18n.js
+++ mega-cart/frontend/src/i18n.js
@@ -1,16 +1,16 @@
 import { createI18n } from "vue-i18n";
-// import ar from "./locales/ar.json";
+import ar from "./locales/ar.json";
 import en from "./locales/en.json";
-// import es from "./locales/es.json";
-// import fr from "./locales/fr.json";
-// import hi from "./locales/hi.json";
+import es from "./locales/es.json";
+import fr from "./locales/fr.json";
+import hi from "./locales/hi.json";
 import zh from "./locales/zh.json";
 
 const saved = localStorage.getItem("lang");
 
 export const i18n = createI18n({
   legacy: false,
-  locale: saved || "zh",
+  locale: saved || "en",
   fallbackLocale: "en",
-  messages: {en, zh}//{ar, en, es, fr, hi, zh}
+  messages: {ar, en, es, fr, hi, zh}
 });
